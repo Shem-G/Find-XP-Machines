4aece7ebb75c298747a80ad6a47f23c8f2f2089c
Shem <sgyll@protonmail.com>
2019-07-16 20:28:55 +0100

    Upload

---
+++ FindXPMachines.ps1
@@ -0,0 +1,29 @@
+﻿$computers = Get-ADComputer -Filter 'OperatingSystem -like "*xp*"' -Properties name, operatingsystem, operatingsystemversion | select name, operatingsystem, operatingsystemversion
+$outputObj =@()
+$offline =@()
+
+foreach ($comp in $computers){
+
+$testnet = ''
+
+    #Tries to ping the computer, then stores results in variable $testnet
+    try
+    {
+        $testnet = Test-Connection $comp.name -Count 2 -ErrorAction Stop
+    }
+    #If unable to ping the computer, it displays that it is not connected.
+    catch
+    {
+    $offline += $comp
+    }
+
+    #If ping was successful, the script moves on, if not it loops back to the next computer
+    if($testnet)
+    {
+    $comp
+    $outputObj += $comp
+    }
+    }
+
+    $outputObj
+    $offline
